4d1e9ef55783b5b7314c638902b7721fbbd26f18
KalebGrove <kwolfgrove@gmail.com>
2024-04-22 14:55:40 +0200

    Completed Exercise

--- PRO2/Exams/D3_T1/X13400/solucio.cc
+++ PRO2/Exams/D3_T1/X13400/solucio.cc
@@ -10,52 +10,45 @@ void Cjt_estudiants::afegir_estudiant(const Estudiant &est, bool& b) {
     int dni = est.consultar_DNI();
 
     int pos = cerca_dicot(vest, 0, i, dni);
+
+    b = (dni == vest[pos].consultar_DNI());
     
-    if(dni != vest[pos].consultar_DNI()) {
+    if(!b) {
         while (i >= pos) {
                 vest[i+1]=vest[i];
                 --i;
         }
-        vest[pos] = est;
+        vest[i+1] = est;
         ++nest;
-        b = false;
-    } 
-    else {
-        b = true;
-    }
+        
+        bool has_grade = est.te_nota();
 
-    bool has_grade = est.te_nota();
-
-    if(has_grade) {
-        int grade = est.consultar_nota();
-        if(grade == 10) intervals[9]--;
-        else intervals[grade]++;
-    }
+        if(has_grade) {
+            incrementar_interval(est.consultar_nota());
+        }
+    }   
 }
 
 void Cjt_estudiants::esborrar_estudiant(int dni, bool& b) {
     int pos = cerca_dicot(vest, 0, nest-1, dni);
 
-    if(dni == vest[pos].consultar_DNI()) {
-        for(int i = pos; i < nest-1; i++) {
+    b = (pos < nest and dni == vest[pos].consultar_DNI());
+
+    if(b) {
+
+        Estudiant est = vest[pos];
+        bool has_grade = est.te_nota();
+
+        if(has_grade) {
+            decrementar_interval(est.consultar_nota());
+        }
+
+        for(int i = pos; i < nest; i++) {
             vest[i] = vest[i+1];
         }
         nest--;
-        b = true;
-    }
-    else {
-        b = false;
     }
 
-    Estudiant est = vest[pos];
-
-    bool has_grade = est.te_nota();
-
-    if(has_grade) {
-        int grade = est.consultar_nota();
-        if(grade == 10) intervals[9]--;
-        else intervals[grade]--;
-    }
 }
 
 void Cjt_estudiants::incrementar_interval(double x) {
