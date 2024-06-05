68f5fd6ea72c73c22901d592731c90db27a26989
KalebGrove <kwolfgrove@gmail.com>
2024-06-05 15:58:01 +0200

    updates

--- PRO2/ClassesArbre/X09609/program.hh
+++ PRO2/ClassesArbre/X09609/program.hh
@@ -1,29 +1,49 @@
-void sub_arrel_aux(Arbre& asub, node_arbre* n, const T& x, bool& found) {
-    //Base case
-    if(n == NULL) {
-        return;
-    }
-    
-    if(not found) {
-        if(n->info == x) {
-            found = true;
-            asub.primer_node = copia_node_arbre(n);
-            return;
-        }
-        else {
-            sub_arrel_aux(asub, n->segE, x, found);
-            if(not found) sub_arrel_aux(asub, n->segD, x, found);
-            return;
-        }
-    }
-
-    return;
-}
-
-void sub_arrel(Arbre& asub, const T& x) {
-    bool found = false;
-    sub_arrel_aux(asub, asub.primer_node, x, found);
+void sub_arrel(Arbre& asub, const T& x){
+    int a = 0;
+    asub.primer_node = copia_node_arbre(aux(this->primer_node, x, a));
 }
  /* Pre: p.i. = A, asub es buit */
  /* Post: si A conte x, asub es el subarbre d'A resultat de la cerca;
     si A no conte x, asub es buit */
+
+node_arbre* aux(node_arbre* p, const T& x, int& c){
+    if (p==nullptr)
+    {
+        return nullptr;
+    }
+    if (p->info == x)
+    {
+        return p;
+    }
+    
+
+    c++;
+    int c1 = c;
+    int c2 = c;
+
+    node_arbre* p1 = aux(p->segE, x, c1);
+    node_arbre* p2 = aux(p->segD, x, c2);
+
+    if (p1==nullptr and p2==nullptr)
+    {
+        return nullptr;
+    }
+    if (p1==nullptr or p2==nullptr)
+    {
+        if (p1==nullptr)
+        {
+            return p2;
+        }
+        
+        return p1;
+    }
+    
+    if (c1>c2)
+    {
+        return p2;
+    }
+    
+    return p1;
+    
+    
+}
